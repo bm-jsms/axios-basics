06412e5fe0a4b0e9870f23e06668b983d2bb57bb
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-18 15:23:37 -0500

    axios fetch

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+function getUsers() {
+	axios({
+		url: 'https://jsonplaceholder.typicode.com/users',
+		method: 'GET',
+		params: {
+			_limit: 5,
+		},
+	})
+		.then(res => {
+			console.log(res.data);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+}
+
+getUsers();
