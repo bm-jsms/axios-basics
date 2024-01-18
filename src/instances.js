30c42b33c86c1f70f8e6391701dd0f01f65da56d
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-18 17:04:55 -0500

    axios - instance

---
+++ src/instances.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+async function main() {
+	const axiosInstance = axios.create({
+		baseURL: 'https://jsonplaceholder.typicode.com',
+	});
+
+	const resPosts = await axiosInstance.get('/posts/1');
+	console.log(resPosts);
+
+	const resUsers = await axiosInstance.get('/users/1');
+	console.log(resUsers);
+}
+
+main();
