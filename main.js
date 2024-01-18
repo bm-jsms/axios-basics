8c756297454f88ef4feb5d53632053d801506f99
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-18 15:37:44 -0500

    axios - post - v2

--- main.js
+++ main.js
@@ -14,14 +14,19 @@ function postUsers() {
 	// 		console.log(err);
 	// 	});
 
-	axios({
-		method: 'post',
-		url: 'https://reqres.in/api/register',
-		data: {
+	// axios({
+	// 	method: 'post',
+	// 	url: 'https://reqres.in/api/register',
+	// 	data: {
+	// 		email: 'eve.holt@reqres.in',
+	// 		password: 'pistol',
+	// 	},
+	// })
+	axios
+		.post('https://reqres.in/api/register', {
 			email: 'eve.holt@reqres.in',
 			password: 'pistol',
-		},
-	})
+		})
 		.then(res => {
 			console.log(res.data);
 		})
